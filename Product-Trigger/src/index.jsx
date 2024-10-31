802645f37d8dba7a341fca28d0a86e2b1a5ff90a
Francisco Javier González Aparicio <fjgonzalez25691@gmail.com>
2024-10-31 09:48:51 +0100

    api-test

--- Product-Trigger/src/index.jsx
+++ Product-Trigger/src/index.jsx
@@ -1,4 +1,28 @@
+//TODO: Add necessary importS to call a REST API
+import api, { route } from "@forge/api";
+
 export async function run(event, context) {
 	//console.log('Hello World!');
 	console.log(event);
+
+	// TODO: Call  the Jira REST API to add comment to the issue.
+	var bodyData = `{
+		"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper."
+		}
+	  }`;
+
+	  var issueIdOrKey = event.issue.id;
+	  
+	  const response = await api.asApp().requestJira(route`/rest/api/2/issue/${issueIdOrKey}/comment`, {
+		method: 'POST',
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json'
+		},
+		body: bodyData
+	  });
+	  
+	  console.log(`Response: ${response.status} ${response.statusText}`);
+	  console.log(await response.json());
 }
+
